882d619d51cfc18c79b2a3a217510ea4d30e1fa8
Renan Fernandes da Silva <renanphernandz@gmail.com>
2021-12-07 20:50:46 -0300

    Leitura básica por coluna

---
+++ LeitorDeImagem/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Tesseract;
+using System.IO;
+
+namespace LeitorDeImagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile;
+            string[] files = Directory.GetFiles(@"C:\Users\renan.fernandes\Desktop\ilovepdf_pages-to-jpg");
+            string directoryLocale = Directory.GetCurrentDirectory();
+            Directory.CreateDirectory(directoryLocale + "\\Retorno");
+            
+
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string pathToSave = directoryLocale + $"\\Retorno\\Retorno da leitura {i+1} - {DateTime.Now.ToString("hhmmss")}.txt";
+                using (StreamWriter sr = new StreamWriter(pathToSave))
+                {
+
+                    using (var ocr = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                    {
+                        sourceFile = files[i];
+                        using (var img = Pix.LoadFromFile(sourceFile))
+                        {
+                            var result = ocr.Process(img);
+
+                            var textResult = result.GetText();
+
+                            sr.Write(textResult);
+
+                            Console.Clear();
+                            Console.WriteLine(Percent(files.Length, i+1).ToString("F2") + "% - " + i + " de " + files.Length);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        static double Percent(int lengthFiles, int i)
+        {
+            double result = (double)(i / lengthFiles) * 100.0;
+
+            return result;
+        }
+    }
+}
